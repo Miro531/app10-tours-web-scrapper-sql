3b6de94027fdc2d942e9871cae3e1891cb91469b
Miro531 <popov.miroslav@hotmail.it>
2024-05-13 18:15:57 +0200

    Day 43: App build

---
+++ main.py
@@ -0,0 +1,54 @@
+import time
+import requests
+import selectorlib
+import smtplib, ssl
+import os
+
+URL = "http://programmer100.pythonanywhere.com/tours/"
+
+def scrape(url):
+    """Scrape the page source from the URL"""
+    response = requests.get(url)
+    source = response.text
+    return source
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "senderpyemail@gmail.com"
+    password = "ositkdjvzjjmwuwy"
+
+    receiver = "popov.miroslav.85@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+            server.login(username, password)
+            server.sendmail(username, receiver, message)
+    print("Email was sent!")
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read_extracted(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+if __name__ == "__main__":
+    while True:
+        scraped = scrape(URL)
+        extracted = extract(scraped)
+        print(extracted)
+
+        content = read_extracted(extracted)
+        if extracted != "No upcoming tours":
+            if extracted not in content:
+                store(extracted)
+                send_email(message="Hey new event was found!!")
+        time.sleep(2)
